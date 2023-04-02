748fcebf82979c8d0102649fc584f86ec4d4937c
Ayush-707 <ayush077999@gmail.com>
2023-04-03 02:29:49 +0530

    slight changes

--- client/src/pages/Authentication/Login.js
+++ client/src/pages/Authentication/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
           <label htmlFor="password" className="block mb-2 font-bold text-gray-800">Password</label>
           <input type="password" placeholder="Enter Your Password" className="w-full px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:shadow-outline-blue focus:border-blue-500" />
         </div>
-        <button onClick={func} className="w-full px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+        <button onClick={func} className="w-full px-4 py-2 text-white bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">
           Login
         </button>
       </form>
