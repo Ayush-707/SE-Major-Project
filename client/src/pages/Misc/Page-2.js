ad900455c0bee40210e7bdce0c8ba5f022ffc6c0
Ayush-707 <ayush077999@gmail.com>
2023-04-10 18:54:46 +0530

    stats added

--- client/src/pages/Misc/Page-2.js
+++ client/src/pages/Misc/Page-2.js
@@ -7,62 +7,63 @@ const Withdraw = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // perform deposit logic here
+    // perform withdraw logic here
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
-      <h1 className="text-2xl font-bold mb-4">Withdraw Money</h1>
-      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 max-w-xxxl">
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="id">
-            Your ID
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="id"
-            type="text"
-            placeholder="Your ID"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
-        </div>
-        
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="date">
-            Date
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="date"
-            type="date"
-            placeholder="Date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 font-bold mb-2" htmlFor="amount">
-            Amount
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="amount"
-            type="number"
-            placeholder="Amount"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-        </div>
-        <div className="flex items-center justify-center">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Withdraw
-          </button>
-        </div>
-      </form>
+      <div className="w-full max-w-md">
+        <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h1 className="text-2xl font-bold mb-4">Withdraw Money</h1>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="id">
+              Your ID
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="id"
+              type="text"
+              placeholder="Your ID"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="date">
+              Date
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="date"
+              type="date"
+              placeholder="Date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="amount">
+              Amount
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="amount"
+              type="number"
+              placeholder="Amount"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Withdraw
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
