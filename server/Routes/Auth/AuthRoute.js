0ed5d7134ddf575fa1cb36255f5e1a90748300fa
Anivesh-69 <106721905+Anivesh-69@users.noreply.github.com>
2023-04-11 16:37:35 +0530

    Update AuthRoute.js

--- server/Routes/Auth/AuthRoute.js
+++ server/Routes/Auth/AuthRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const signup = require( "../../Controllers/Auth/SignupController" ); 
 const login = require( "../../Controllers/Auth/LoginController" ); 
+const Loan = require('../models/Loandb');
 const bodyParser = require('body-parser');
 const path = require('path');
 
@@ -14,9 +15,35 @@ router.post("/User/Signup", signup.userSignUp);
 
 router.post("/User/Login", login.userLogin);
 
+router.get('/loans', (req, res) => {
+  Loan.find()
+    .then(loans => res.json(loans))
+    .catch(error => console.log(error));
+});
 
+router.post('/loans', (req, res) => {
+  const { amount } = req.body;
+  const loan = new Loan({ amount });
+  loan.save()
+    .then(newLoan => res.json(newLoan))
+    .catch(error => console.log(error));
+});
 
-
+router.put('/loans/:id', (req, res) => {
+  const { id } = req.params;
+  const { paymentAmount } = req.body;
+  Loan.findById(id)
+    .then(loan => {
+      loan.payments.push({ amount: paymentAmount });
+      if (loan.amount === loan.payments.reduce((total, p) => total + p.amount, 0)) {
+        loan.status = 'Approved';
+      }
+      loan.save()
+        .then(updatedLoan => res.json(updatedLoan))
+        .catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
+});
 
 
 
